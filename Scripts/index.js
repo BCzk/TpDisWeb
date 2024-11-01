6b8b56770b4842167074158bb38adbf0bcac83e8
SantinoBaldan <santino.bcs@gmail.com>
2024-11-01 00:15:15 -0300

    JavaScript & JQuery Integration + Image slider hover

---
+++ Scripts/index.js
@@ -0,0 +1,23 @@
+/* --- Imágenes Originales --- */
+var firstImgOr = "../Recursos/Juegos/stalkinggaze-image.png";
+var secondImgOr = "../Recursos/Juegos/rotobox-image.png";
+var thirdImgOr = "../Recursos/Juegos/paranoid-image.png";
+
+/* --- Imágenes Hover --- */
+var firstImgHo = "../Recursos/Juegos/stalkinggaze-hover.png";
+var secondImgHo = "../Recursos/Juegos/rotobox-hover.png";
+var thirdImgHo = "../Recursos/Juegos/paranoid-hover.png";
+
+/* --- Mouse Enter --- */
+$(".lista-fotos").mouseenter(function () {
+    $("#img1").attr("src", firstImgHo);
+    $("#img2").attr("src", secondImgHo);
+    $("#img3").attr("src", thirdImgHo);
+});
+
+/* --- Mouse Leave --- */
+$(".lista-fotos").mouseleave(function () {
+    $("#img1").attr("src", firstImgOr);
+    $("#img2").attr("src", secondImgOr);
+    $("#img3").attr("src", thirdImgOr);
+});
