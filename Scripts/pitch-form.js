80823f65976ac08f68e05a05e7c2d0e9662deb88
SantinoBaldan <santino.bcs@gmail.com>
2024-11-01 11:54:42 -0300

    Formularios JavaScript DONEEEEE

--- Scripts/pitch-form.js
+++ Scripts/pitch-form.js
@@ -1,6 +1,6 @@
 $("#pitch-form").on('submit',function (event) {
     var subject = document.getElementById("form-subject").value;
-    var email = document.getElementById("form-email").value;
+    var email = document.getElementById("pitch-form-email").value;
 
     alert("The email '" + subject + "' created by " + email + " has been sent succesfully.");
 });
